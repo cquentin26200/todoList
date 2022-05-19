8645957898c62d6c8850c8c89e4271a19e3bedd7
cquentin26200 <77721932+cquentin26200@users.noreply.github.com>
2022-05-19 02:47:38 +0200

    add the function only select and the function only not select

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@ const basicSentence = document.querySelector(".basicSentence");
 
 function addNewTodo() {
   if (inputText.value !== "" && secondInput.value !== "") {
-    if (isNaN(secondInput.value)) {
+    if (!isNaN(secondInput.value)) {
       idea.insertAdjacentHTML(
         "beforeend",
-        `<div class="border justify-content-between m-4 px-4 py-2 d-flex align-items-center">
-        <p class="m-0 text-start">Name: ${inputText.value} Number: ${secondInput.value}</p>
+        `<div class="border justify-content-between m-4 px-4 d-flex align-items-center">
+        <p class="m-0 text-start">Name: ${inputText.value}</p>
         <div class="d-flex align-items-center">
+        <p class ="m-0 me-4 p-3 border border-top-0 border-bottom-0">${secondInput.value}</p>
         <input class="form-check-input select mt-0 me-4" type="checkbox">
         <img src="../img/trash.svg" class="cursor-pointer trashCan" alt="trash can" />
         </div>`
@@ -28,15 +29,20 @@ function addNewTodo() {
     });
   });
 
+  const delSelected = document.querySelector(".delSelected");
+  const deleteSelected = document.querySelector(".deleteSelected");
   const selected = document.querySelectorAll(".select");
 
   selected.forEach((element) => {
     element.addEventListener("click", () => {
-      if (element.classList.contains("selected")) {
-        element.classList.remove("selected");
-      } else {
+      if (element.checked == true) {
         element.classList.add("selected");
+      } else {
+        element.classList.remove("selected");
       }
+
+      delSelected.checked = false;
+      deleteSelected.checked = false;
     });
   });
 
@@ -52,19 +58,60 @@ function addNewTodo() {
     });
   });
 
-  const delSelected = document.querySelector(".delSelected");
+  delSelected.addEventListener("click", () => {
+    selected.forEach((element) => {
+      element.checked = false;
+    });
+  });
 
-  delSelected.addEventListener("click", () => {});
+  deleteSelected.addEventListener("click", () => {
+    selected.forEach((element) => {
+      if (element.checked == true) {
+        element.parentElement.parentElement.remove();
+      }
+    });
+  });
+
   inputText.value = "";
   secondInput.value = "";
+  delSelected.checked = false;
+  deleteSelected.checked = false;
 }
 
 inputSubmit.addEventListener("click", addNewTodo);
 
-const filter = document.querySelectorAll("ul li");
+function hideTheNotSelect() {
+  const selected = document.querySelectorAll(".select");
+  selected.forEach((element) => {
+    if (
+      !element.parentElement.parentElement.classList.contains("d-none") &&
+      element.checked === false
+    ) {
+      element.parentElement.parentElement.classList.add("d-none");
+    } else {
+      element.parentElement.parentElement.classList.remove("d-none");
+    }
+  });
+}
 
-filter.forEach((element) => {
-  element.style.display = "flex";
-  element.style.alignItems = "center";
-  element.children[0].style.marginLeft = "5px";
-});
+const onlySelect = document.querySelector(".onlySelect");
+
+onlySelect.addEventListener("click", hideTheNotSelect);
+
+function hideTheSelect() {
+  const selected = document.querySelectorAll(".select");
+  selected.forEach((element) => {
+    if (
+      !element.parentElement.parentElement.classList.contains("d-none") &&
+      element.checked === true
+    ) {
+      element.parentElement.parentElement.classList.add("d-none");
+    } else {
+      element.parentElement.parentElement.classList.remove("d-none");
+    }
+  });
+}
+
+const onlyNotSelect = document.querySelector(".onlyNotSelect");
+
+onlyNotSelect.addEventListener("click", hideTheSelect);
