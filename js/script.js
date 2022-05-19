5d464e65504dd6820f122559eafc79f818d9049b
cquentin26200 <77721932+cquentin26200@users.noreply.github.com>
2022-05-19 04:11:39 +0200

    add verification for a number, change the checkbox

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 const inputText = document.querySelector(".firstInput");
 const secondInput = document.querySelector(".secondInput");
-const inputSubmit = document.querySelector(".btn-outline-secondary");
 const idea = document.querySelector(".idea");
 const basicSentence = document.querySelector(".basicSentence");
 
+const delSelected = document.querySelector(".delSelected");
+const deleteSelected = document.querySelector(".deleteSelected");
+const selectAll = document.querySelector(".selectAll");
+const onlySelect = document.querySelector(".onlySelect");
+const onlyNotSelect = document.querySelector(".onlyNotSelect");
+
 function addNewTodo() {
   if (inputText.value !== "" && secondInput.value !== "") {
-    if (!isNaN(secondInput.value)) {
+    let name = inputText.value.charAt(0).toUpperCase()+inputText.value.slice(1);
+    if (!isNaN(secondInput.value) && isNaN(inputText.value)) {
       idea.insertAdjacentHTML(
         "beforeend",
         `<div class="border justify-content-between m-4 px-4 d-flex align-items-center">
-        <p class="m-0 text-start">Name: ${inputText.value}</p>
+        <p class="m-0 text-start">Name: ${secondInput.value > 1 ? name+"s" : name}</p>
         <div class="d-flex align-items-center">
         <p class ="m-0 me-4 p-3 border border-top-0 border-bottom-0">${secondInput.value}</p>
         <input class="form-check-input select mt-0 me-4" type="checkbox">
@@ -21,64 +27,70 @@ function addNewTodo() {
     }
   }
 
-  const trashCan = document.querySelectorAll(".trashCan");
-
-  trashCan.forEach((element) => {
-    element.addEventListener("click", () => {
-      element.parentElement.parentElement.remove();
-    });
-  });
-
-  const delSelected = document.querySelector(".delSelected");
-  const deleteSelected = document.querySelector(".deleteSelected");
   const selected = document.querySelectorAll(".select");
+  const btnSelect = document.querySelectorAll(".btnSelect");
 
   selected.forEach((element) => {
     element.addEventListener("click", () => {
-      if (element.checked == true) {
-        element.classList.add("selected");
-      } else {
-        element.classList.remove("selected");
-      }
-
-      delSelected.checked = false;
-      deleteSelected.checked = false;
-    });
-  });
-
-  const selectAll = document.querySelector(".selectAll");
-
-  selectAll.addEventListener("click", () => {
-    selected.forEach((element) => {
-      if (selectAll.checked == true) {
-        element.checked = true;
-      } else {
+      btnSelect.forEach((element) => {
         element.checked = false;
-      }
+      });
     });
   });
-
-  delSelected.addEventListener("click", () => {
-    selected.forEach((element) => {
-      element.checked = false;
-    });
-  });
-
-  deleteSelected.addEventListener("click", () => {
-    selected.forEach((element) => {
-      if (element.checked == true) {
-        element.parentElement.parentElement.remove();
-      }
-    });
-  });
-
+  
   inputText.value = "";
   secondInput.value = "";
   delSelected.checked = false;
   deleteSelected.checked = false;
+  selectAll.checked = false;
+  onlySelect.checked = false;
+  onlyNotSelect.checked = false;
 }
 
+const inputSubmit = document.querySelector(".btn-outline-secondary");
+
 inputSubmit.addEventListener("click", addNewTodo);
+
+const trashCan = document.querySelectorAll(".trashCan");
+
+trashCan.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.parentElement.parentElement.remove();
+  });
+});
+
+function selectAllElement() {
+  const selected = document.querySelectorAll(".select");
+  selected.forEach((element) => {
+    if (selectAll.checked == true) {
+      element.checked = true;
+    } else {
+      element.checked = false;
+    }
+  });
+}
+
+selectAll.addEventListener("click", selectAllElement);
+
+function deleteAllSelected() {
+  const selected = document.querySelectorAll(".select");
+  selected.forEach((element) => {
+    element.checked = false;
+  });
+}
+
+delSelected.addEventListener("click", deleteAllSelected);
+
+function deleteAllElement() {
+  const selected = document.querySelectorAll(".select");
+  selected.forEach((element) => {
+    if (element.checked == true) {
+      element.parentElement.parentElement.remove();
+    }
+  });
+}
+
+deleteSelected.addEventListener("click", deleteAllElement);
 
 function hideTheNotSelect() {
   const selected = document.querySelectorAll(".select");
@@ -93,8 +105,6 @@ function hideTheNotSelect() {
     }
   });
 }
-
-const onlySelect = document.querySelector(".onlySelect");
 
 onlySelect.addEventListener("click", hideTheNotSelect);
 
@@ -112,6 +122,16 @@ function hideTheSelect() {
   });
 }
 
-const onlyNotSelect = document.querySelector(".onlyNotSelect");
-
 onlyNotSelect.addEventListener("click", hideTheSelect);
+
+const showAll = document.querySelector(".showAll");
+
+  showAll.addEventListener("click", () => {
+    const selected = document.querySelectorAll(".select");
+    selected.forEach((element) => {
+      element.parentElement.parentElement.classList.remove("d-none");
+    });
+  });
+
+
+  
